17d4f0d6ff75cfb28626405c84f4f223b175b078
Kuksov Artem <35718107+temakuksov@users.noreply.github.com>
2023-04-17 20:36:35 +0500

    java core - Классы + конструкторы + геттеры и сеттеры

--- src/lesson_9/myNinthClass.java
+++ src/lesson_9/myNinthClass.java
@@ -3,7 +3,7 @@ package lesson_9;
 public class myNinthClass {
     public static void main(String[] args) {
         int f = 5; // размер игрового поля
-        int l = 4; // длина ряда для победы
+        int l = 4; // длина ряда для победы (l<=f)
         myTicTacToe ticTacToe = new myTicTacToe(f, l);
         ticTacToe.playGame(f, l);
     }
