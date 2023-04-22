266b6b2daf2e939dd5edd0952d3e26fee175f758
Kuksov Artem <35718107+temakuksov@users.noreply.github.com>
2023-04-22 17:13:59 +0500

    java core - ООП. Инкапсуляция + Наследование

---
+++ src/lesson_10/myVideoDisk.java
@@ -0,0 +1,9 @@
+package lesson_10;
+
+public class myVideoDisk extends myDisk {
+    // класс фильмов (название, год выхода, студия издатель, режиссер, продолжительность в минутах)
+    private String producer;
+    private String director;
+    private int minutesCount;
+
+}
