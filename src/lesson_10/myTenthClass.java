55a061a43d5a46c12bf43f6683585a7240921f80
Kuksov Artem <35718107+temakuksov@users.noreply.github.com>
2023-04-24 02:51:36 +0500

    java core - ООП. Инкапсуляция + Наследование

--- src/lesson_10/myTenthClass.java
+++ src/lesson_10/myTenthClass.java
@@ -1,11 +1,45 @@
 package lesson_10;
 
+import java.sql.SQLOutput;
+
 public class myTenthClass {
     // кличка животного
     private String nName;
     private String food;
     private String location;
 
+    public void setnName(String nName) {
+        this.nName = nName;
+    }
 
+    public void setFood(String food) {
+        this.food = food;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getnName() {
+        return nName;
+    }
+
+    public String getFood() {
+        return food;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void makeNoise() {
+    }
+
+    public void eat() {
+    }
+
+    public void sleep() {
+        System.out.println();
+    }
 
 }
