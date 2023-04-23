a3770557dc3430cd4513d553b94d7e2e08808255
Kuksov Artem <35718107+temakuksov@users.noreply.github.com>
2023-04-23 21:06:12 +0500

    java core - ООП. Инкапсуляция + Наследование

--- src/lesson_10/main.java
+++ src/lesson_10/main.java
@@ -16,6 +16,21 @@ public class main {
         myAudioDisk audioDisk2 = new myAudioDisk();
         myAudioDisk audioDisk3 = new myAudioDisk();
 
+        audioDisk1.setName("White album");
+        audioDisk1.setYear(1991);
+        audioDisk1.setSigner("Kino");
+        audioDisk1.setTracksCount(14);
+
+        audioDisk2.setSigner("Kirpichi");
+        audioDisk2.setName("Sila uma");
+        audioDisk2.setYear(2001);
+        audioDisk2.setTracksCount(15);
+
+        audioDisk3.setSigner("Sektor Gaza");
+        audioDisk3.setName("Kolhoznyi pank");
+        audioDisk3.setYear(1992);
+        audioDisk3.setTracksCount(17);
+
 
     }
 }
