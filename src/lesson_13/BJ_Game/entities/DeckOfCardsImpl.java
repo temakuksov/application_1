876e94aee4947be8d90301e100ac4e606cee4e51
Kuksov Artem <35718107+temakuksov@users.noreply.github.com>
2023-06-20 02:30:17 +0500

    java core - ООП и BlackJack

--- src/lesson_13/BJ_Game/entities/DeckOfCardsImpl.java
+++ src/lesson_13/BJ_Game/entities/DeckOfCardsImpl.java
@@ -7,7 +7,16 @@ public class DeckOfCardsImpl implements DeckOfCards {
     Card[] cards = new Card[52];
 
     public DeckOfCardsImpl() {
-        //
+        char[] cardSuits = new char[]{"♥", "♦", "♣", "♠"};
+        int[] cardValues;
+        cardValues = new int[]{2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11};
+        String[] cardNames = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "В", "Д", "К", "Т"};
+        for (int i = 0; i < cardSuits.length; i++) {
+            for (int j = 0; j < cardValues.length; j++) {
+                cards[i * 13 + j] = new CardImpl(cardValues[j],cardNames[j].concat(cardSuits[i]));
+            }
+        }
+
     }
 
     @Override
