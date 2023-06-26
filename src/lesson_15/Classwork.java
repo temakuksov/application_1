441d445832c6b98b6b225896663f39b4051f5d7a
Kuksov Artem <35718107+temakuksov@users.noreply.github.com>
2023-06-27 01:05:13 +0500

    java core - ArrayList

--- src/lesson_15/Classwork.java
+++ src/lesson_15/Classwork.java
@@ -1,4 +1,44 @@
 package lesson_15;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+
 public class Classwork {
+    public static void main(String[] args) {
+        // 1 - создание коллекции и добавление в нее элементов
+        HashSet<String> strings = new HashSet<>();
+
+        strings.add("AAA");
+        strings.add("BBB");
+        strings.add("BBB");
+        strings.add("BBB");
+        strings.add("CCC");
+        strings.add("DDD");
+        strings.add("DDD");
+        strings.add("DDD");
+        strings.add("EEE");
+        strings.add("EEE");
+        System.out.println("HashSet не иммет порядка вхождения и не хранит дубли");
+
+        // 2 - LinkedHashSet.
+        LinkedHashSet<String> stringLinkedHashSet = new LinkedHashSet<>();
+        stringLinkedHashSet.add("AAA");
+        stringLinkedHashSet.add("BBB");
+        stringLinkedHashSet.add("CCC");
+        stringLinkedHashSet.add("CCC");
+        stringLinkedHashSet.add("DDD");
+        stringLinkedHashSet.add("DDD");
+        stringLinkedHashSet.add("EEE");
+        stringLinkedHashSet.add("EEE");
+        stringLinkedHashSet.add("EEE");
+
+        System.out.println("LinkedHashSet имеет порядок вхождения, но не хранит дубли");
+
+        for (Iterator<String> iter = stringLinkedHashSet.iterator(); iter.hasNext();) {
+            System.out.println(iter.next());
+
+        }
+        System.out.println("");
+    }
 }
