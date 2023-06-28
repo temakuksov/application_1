4150b21cac6e827840d27648e1db6ae3cb74a44a
Kuksov Artem <35718107+temakuksov@users.noreply.github.com>
2023-06-29 02:12:56 +0500

    java core - HashMap

---
+++ src/lesson_17/homework/Task02.java
@@ -0,0 +1,10 @@
+package lesson_17.homework;
+
+public class Task02 {
+    public static void main(String[] args) {
+        // Задача 2
+        // Программа вводит с клавиатуры имя месяца и выводит его номер на экран в виде: "May is the 5 month".
+        // Используй коллекции.
+
+    }
+}
---
+++ src/lesson_17/homework/Task03.java
@@ -0,0 +1,39 @@
+package lesson_17.homework;
+
+public class Task03 {
+    public static void main(String[] args) {
+        // Задача 3
+        // Задача: Программа определяет, какая семья (фамилию) живёт в доме с указанным номером.
+        // Новая задача: Программа должна работать не с номерами домов, а с городами:
+        // Пример ввода:
+        //   Москва -  Ивановы
+        //   Киев - Петровы
+        //   Лондон - Абрамовичи
+        //
+        // Пример ввода: Лондон
+        // Пример вывода: Абрамовичи
+        //
+        // public static void main(String[] args) throws IOException {
+        //        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        //        Map<String, String> map = new HashMap<>();
+        //        while (true) {
+        //            String city = reader.readLine();
+        //            if (city.isEmpty()) {
+        //                break;
+        //            }
+        //            String family = reader.readLine();
+        //
+        //            map.put(city, family);
+        //        }
+        //
+        //        String city = reader.readLine();
+        //
+        //        if (map.containsKey(city)) {
+        //            String familyName = map.get(city);
+        //            System.out.println(familyName);
+        //        }
+        //    }
+
+
+    }
+}
