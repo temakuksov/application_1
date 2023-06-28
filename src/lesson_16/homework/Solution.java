42676ddb9f43296ee9314c5538998608281d9875
Kuksov Artem <35718107+temakuksov@users.noreply.github.com>
2023-06-29 01:35:56 +0500

    java core - TreeSet

--- src/lesson_16/homework/Solution.java
+++ src/lesson_16/homework/Solution.java
@@ -14,20 +14,23 @@ public class Solution {
     // - Метод removeAllNumbersGreaterThan10() должен удалять из множества все числа больше 10.
 
     public static Set<Integer> createSet() {
-        // Random rnd = new Random();
-        int rnd;
         Set<Integer> trSet = new LinkedHashSet<>();
         while (trSet.size() < 20) {
-            rnd = (int) (Math.random() * 50);
-            trSet.add(rnd);
+            trSet.add((int) (Math.random() * 20));  // решил использовать класс Math вместо Random
         }
         return trSet;
     }
 
+    public static Set<Integer> removeAllNumbersGreaterThan10(Set<Integer> localSet) {
+        localSet.removeIf(a -> a > 10);
+        return localSet;
+    }
 
     public static void main(String[] args) {
         Set<Integer> ints = createSet();
         System.out.println(ints);
-    }
+        System.out.println("Результат (множество где удалены все числа больше 10):");
+        System.out.println(removeAllNumbersGreaterThan10(ints));
 
+    }
 }
