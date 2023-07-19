48435aa9413fd3ee2afd99b8f758eccf6f77eaaf
Kuksov Artem <35718107+temakuksov@users.noreply.github.com>
2023-07-19 19:16:57 +0500

    java core - HashMap

--- src/lesson_17/homework/Task01.java
+++ src/lesson_17/homework/Task01.java
@@ -1,5 +1,8 @@
 package lesson_17.homework;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Task01 {
     public static void main(String[] args) {
         // Задача 1
@@ -8,5 +11,20 @@ public class Task01 {
         // 3. Пусть среди этих 10 человек есть люди с одинаковыми фамилиями.
         // 4. Вывести содержимое Map на экран.
 
+        HashMap<String, String> mans = new HashMap<>();
+        mans.put("Туголуков", "Антон");
+        mans.put("Светлаков", "Никита");
+        mans.put("Москвина", "Светлана");
+        mans.put("Фокин", "Артём");
+        mans.put("Куксов", "Артём");
+        mans.put("Петров", "Никита");
+        mans.put("Стельцова", "Светлана");
+        mans.put("Решетников", "Антон");
+        mans.put("Струев", "Никита");
+        mans.put("Сидоров", "Антон");
+
+        for (Map.Entry<String, String> m : mans.entrySet()) {
+            System.out.println(m.getKey()+ " - "+m.getValue());
+        }
     }
 }
--- src/lesson_17/homework/Task02.java
+++ src/lesson_17/homework/Task02.java
@@ -1,10 +1,31 @@
 package lesson_17.homework;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Task02 {
     public static void main(String[] args) {
         // Задача 2
         // Программа вводит с клавиатуры имя месяца и выводит его номер на экран в виде: "May is the 5 month".
         // Используй коллекции.
+        Map<String, Integer> months = new HashMap<>();
+        months.put("january", 1);
+        months.put("february", 2);
+        months.put("march", 3);
+        months.put("april", 4);
+        months.put("may", 5);
+        months.put("june", 6);
+        months.put("july", 7);
+        months.put("august", 8);
+        months.put("september", 9);
+        months.put("october", 10);
+        months.put("november", 11);
+        months.put("december", 12);
 
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите название месяца\n (january, february, march, april, may, june, july, august, september, october, november, december): ");
+        String mnth = scan.next().toLowerCase();
+        System.out.println(months.get(mnth));
     }
 }
