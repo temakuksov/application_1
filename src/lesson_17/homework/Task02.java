60400a956d73495d2c1c3af7f823d17a5c177761
Kuksov Artem <35718107+temakuksov@users.noreply.github.com>
2023-07-20 00:28:34 +0500

    java core - HashMap

--- src/lesson_17/homework/Task02.java
+++ src/lesson_17/homework/Task02.java
@@ -9,23 +9,27 @@ public class Task02 {
         // Задача 2
         // Программа вводит с клавиатуры имя месяца и выводит его номер на экран в виде: "May is the 5 month".
         // Используй коллекции.
-        Map<String, Integer> months = new HashMap<>();
-        months.put("january", 1);
-        months.put("february", 2);
-        months.put("march", 3);
-        months.put("april", 4);
-        months.put("may", 5);
-        months.put("june", 6);
-        months.put("july", 7);
-        months.put("august", 8);
-        months.put("september", 9);
-        months.put("october", 10);
-        months.put("november", 11);
-        months.put("december", 12);
+        Map<String, Integer> year = new HashMap<>();
+        year.put("january", 1);
+        year.put("february", 2);
+        year.put("march", 3);
+        year.put("april", 4);
+        year.put("may", 5);
+        year.put("june", 6);
+        year.put("july", 7);
+        year.put("august", 8);
+        year.put("september", 9);
+        year.put("october", 10);
+        year.put("november", 11);
+        year.put("december", 12);
 
         Scanner scan = new Scanner(System.in);
         System.out.println("Введите название месяца\n (january, february, march, april, may, june, july, august, september, october, november, december): ");
         String mnth = scan.next().toLowerCase();
-        System.out.println(months.get(mnth));
+        if (year.get(mnth) != null) {
+            System.out.printf("%s is the %s month", mnth, year.get(mnth));
+        } else {
+            System.out.printf("month %s - is noe exist!", mnth);
+        }
     }
 }
