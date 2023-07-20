3d361621d71a6c4dc82288a1bbce7280a8affa06
Kuksov Artem <35718107+temakuksov@users.noreply.github.com>
2023-07-20 20:45:52 +0500

    java core - HashMap

--- src/lesson_17/homework/Task03.java
+++ src/lesson_17/homework/Task03.java
@@ -1,9 +1,14 @@
 package lesson_17.homework;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Task03 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // Задача 3
-        // Задача: Программа определяет, какая семья (фамилию) живёт в доме с указанным номером.
         // Новая задача: Программа должна работать не с номерами домов, а с городами:
         // Пример ввода:
         //   Москва -  Ивановы
@@ -22,18 +27,38 @@ public class Task03 {
         //                break;
         //            }
         //            String family = reader.readLine();
-        //
         //            map.put(city, family);
         //        }
-        //
         //        String city = reader.readLine();
-        //
         //        if (map.containsKey(city)) {
         //            String familyName = map.get(city);
         //            System.out.println(familyName);
         //        }
         //    }
 
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Map<String, String> mapTF = new HashMap<>();
 
+        while (true) {
+            System.out.println("Введите название города: ");
+            String city = reader.readLine();
+            if (city.isEmpty()) {
+                System.out.println("Ввод завершен!");
+                break;
+            }
+            System.out.println("Введите фамилию, проживающих в городе " + city + ": ");
+            String surname = reader.readLine();
+            mapTF.put(city, surname);
+        }
+
+        System.out.println("Введите название города: ");
+        String city = reader.readLine();
+
+        if (mapTF.containsKey(city)) {
+            String familyName = mapTF.get(city);
+            System.out.println("В городе " + city + " проживают с фамилией " + familyName);
+        } else {
+            System.out.println("по нашим данным в городе " + city + " никто не проживает!");
+        }
     }
 }
