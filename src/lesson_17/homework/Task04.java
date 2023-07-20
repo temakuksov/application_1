8dfa2663fedab116da5bc89cdb1ac7d1225b1041
Kuksov Artem <35718107+temakuksov@users.noreply.github.com>
2023-07-20 21:35:20 +0500

    java core - HashMap

--- src/lesson_17/homework/Task04.java
+++ src/lesson_17/homework/Task04.java
@@ -1,5 +1,8 @@
 package lesson_17.homework;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Task04 {
     public static void main(String[] args) {
         // Задача 4
@@ -17,7 +20,19 @@ public class Task04 {
         // Вывести содержимое коллекции на экран, каждый элемент с новой строки.
         // Пример вывода (тут показана только одна строка):
         // картофель - клубень
-
-
+        Map<String, String> vegs = new HashMap<>();
+        vegs.put("арбуз", "ягода");
+        vegs.put("банан", "трава");
+        vegs.put("вишня", "ягода");
+        vegs.put("груша", "фрукт");
+        vegs.put("дыня", "овощь");
+        vegs.put("ежевика", "куст");
+        vegs.put("жень-шень", "корень");
+        vegs.put("земляника", "ягода");
+        vegs.put("ирис", "цветок");
+        vegs.put("картофель", "клубень");
+        for(Map.Entry<String,String> v : vegs.entrySet()){
+            System.out.println(v.getKey()+" - "+v.getValue());
+        }
     }
 }
--- src/lesson_17/homework/Task05.java
+++ src/lesson_17/homework/Task05.java
@@ -1,5 +1,8 @@
 package lesson_17.homework;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Task05 {
     public static void main(String[] args) {
         // Задача 5
@@ -21,7 +24,20 @@ public class Task05 {
         //      map.put("Comp", (double) 123);
         //     //напишите тут ваш код
         //  }
+        Map<String, Object> map = new HashMap<>();
 
-
+        map.put("Sim", 5);
+        map.put("Tom", 5.5);
+        map.put("Arbus", false);
+        map.put("Baby", null);
+        map.put("Cat", "Cat");
+        map.put("Eat", 56L);
+        map.put("Food", '3');
+        map.put("Gevey", '6');
+        map.put("Hugs", 111111111111L);
+        map.put("Comp", (double) 123);
+        for (Map.Entry<String, Object> mObj : map.entrySet()) {
+            System.out.println(mObj.getKey()+" - "+mObj.getValue());
+        }
     }
 }
